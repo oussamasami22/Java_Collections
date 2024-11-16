ab32d2ecd76b7acf4c2ea14f162cdd0e65cd0930
OUSSAMA ELG <oussama.elgoum@gmail.com>
2024-11-16 21:15:01 +0100

    hashset methods

--- hashSet.java
+++ hashSet.java
@@ -10,6 +10,15 @@ public class hashSet {
         friends.add("elgoum"); //not added
 
         System.out.println("my hashSet: "+friends);
+        System.out.println("--------------------------------");
+        friends.remove("elgoum");
+        System.out.println("my hashSet: "+friends);
+        System.out.println("Contient oussama ? " + friends.contains("oussama"));
+
+        for (String friend : friends){
+            System.out.println(friend);
+
+        }
     }
     
 }
